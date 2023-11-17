214ac795665d24119e67e3497b2f0d018b6d83e5
AMANverma9118 <amanverma9118359330@gmail.com>
2023-11-17 10:26:20 +0530

    hosting

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const appRoute = require('./routes/route.js')
-
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use(bodyParser.json()); // Parses JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: true })); // Parses URL-encoded form data
+
+
+
 // routes
 app.use('/api',appRoute);
 
+app.get('/api',(req,res)=>{
+    res.send('Hello everyone');
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is runing on http://localhost:${PORT}`)
+    
 })
